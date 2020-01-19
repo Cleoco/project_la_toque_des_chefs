ea2fce5e5ae536585c11105f3882e3346f9aee3d
Cleoco <cleo.cosnier@gmail.com>
2020-01-19 17:40:53 +0100

    Maquettage homepage : ajout 3dernières recettes, catégories et connexion

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Entity\Recipe;
 use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,7 +16,10 @@ class MainController extends AbstractController
     public function index()
     {
         $menu_categories = $this->getDoctrine()->getRepository(Category::class)->findAll();
+        // Méthode findBy qui permet de récupérer les données avec des critères de filtre et de tri
+        $repo = $this->getDoctrine()->getRepository(Recipe::class)->findBy([],['createdAt' => 'desc'],3);
         return $this->render('main/index.html.twig', [
+            'recipes' => $repo,
             'menu_categories'=>$menu_categories 
         ]);
         
